683e6ea8236af47fd3036d38f7570918020fc756
kristof1345 <kristofnemeth1345@gmail.com>
2023-05-15 11:56:42 +0200

    lead

--- main.go
+++ main.go
@@ -6,13 +6,14 @@ import (
 	"github.com/gofiber/fiber"
 	"github.com/jinzhu/gorm"
 	"github.com/kristof1345/CRM/database"
+	"github.com/kristof1345/CRM/lead"
 )
 
 func setUpRoutes(app *fiber.App) {
-	app.Get(GetLeads)
-	app.Get(GetLead)
-	app.Post(NewLead)
-	app.Delete(DeleteLead)
+	app.Get("/api/v1/lead", lead.GetLeads)
+	app.Get("/api/v1/lead:id", lead.GetLead)
+	app.Post("/api/v1/lead", lead.NewLead)
+	app.Delete("/api/v1/lead:id", lead.DeleteLead)
 }
 
 func initDatabase() {
