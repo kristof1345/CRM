829773318a844c25f184d2060921eb0b6df436bc
kristof1345 <kristofnemeth1345@gmail.com>
2023-05-19 22:01:30 +0200

    ok

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gofiber/fiber"
 	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"github.com/kristof1345/CRM/database"
 	"github.com/kristof1345/CRM/lead"
 )
